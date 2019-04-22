90b3d5a3c2a00856e551104842a709af7a19cc43
Brian Roepke <broepke@gmail.com>
2019-04-22 12:00:57 -0700

    Factorization

---
+++ Module01/factorization.py
@@ -0,0 +1,16 @@
+from math import sqrt
+
+n = 100
+
+n2 = int(n/2)
+fn = []
+for x in range(2, n2+1):
+    if n % x == 0:
+        fn.append(x)  # lower member of factor pair
+        fn.append(n/x)  # upper member of factor pair
+print('N has the following factors: ' + str(fn))
+
+for i in range(1, int(n / 2) + 1):
+    for j in range(1, i):
+        if i * j == n:
+            print(j, ' times ', i, ' equals ', n)
